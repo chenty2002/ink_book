63d4d37080b3f32943ef7ec206cb82b6edd8677f
20231207 <20231207@buaa.edu.cn>
2022-08-04 19:34:46 +0800

    Extra Config

---
+++ src/main/java/com/summerProject/ink_book/Config/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.summerProject.ink_book.Config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedMethods("POST", "GET", "PUT", "DELETE")
+                .maxAge(3000);
+    }
+}
